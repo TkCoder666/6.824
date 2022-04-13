f96dd92461267b7ee9df50a6dd8a67e6da2a4697
tk <2774315388@qq.com>
2022-04-13 19:46:05 +0800

    half work of master

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,15 +1,25 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
-
+import (
+	"log"
+	"math"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
 
 type Coordinator struct {
-	// Your definitions here.
-
+	lock     sync.Mutex
+	nMap     int
+	nReduce  int
+	stage    string
+	files    []string
+	toDotask chan Task
+	tasks    map[string]Task
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -23,7 +33,13 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
+func (c *Coordinator) getTaskLabel(tasktype string, taskid int) string {
+	return tasktype + "-" + strconv.Itoa(taskid)
+}
 
+func (c *Coordinator) ApplyForTask(args *ApplyForTaskArgs, reply *ApplyForTaskReply) error {
+	return nil
+}
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -47,10 +63,7 @@ func (c *Coordinator) server() {
 //
 func (c *Coordinator) Done() bool {
 	ret := false
-
-	// Your code here.
-
-
+	ret = (c.stage == DONE)
 	return ret
 }
 
@@ -60,11 +73,39 @@ func (c *Coordinator) Done() bool {
 // nReduce is the number of reduce tasks to use.
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
-
-	// Your code here.
-
-
+	c := Coordinator{
+		nMap:     len(files),
+		nReduce:  nReduce,
+		stage:    MAP,
+		files:    files,
+		toDotask: make(chan Task, int(math.Max(float64(len(files)), float64(nReduce)))),
+		tasks:    make(map[string]Task),
+	}
+	for i := 0; i < c.nMap; i++ {
+		c.tasks[c.getTaskLabel(MAP, i)] = Task{
+			TaskType: MAP,
+			TaskId:   i,
+			WorkerId: -1,
+			NReduce:  nReduce,
+			NMap:     c.nMap,
+		}
+	}
 	c.server()
+
+	//!check for time do you know
+	go func() {
+		for {
+			time.Sleep(500 * time.Millisecond)
+			c.lock.Lock()
+			for _, task := range c.tasks {
+				if task.WorkerId != -1 && task.TaskType != DONE && task.DeadLine.After(time.Now()) {
+					task.WorkerId = -1
+					c.toDotask <- task
+					log.Printf("send task %v to dotask with timeout", task)
+				}
+			}
+			c.lock.Unlock()
+		}
+	}()
 	return &c
 }
